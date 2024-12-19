4db1258cedc33d8399859e8a6051dea496788884
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-19 12:22:25 -1100

    1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,8 +14,8 @@ const Navbar = () => {
 
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-25">
-      <Link href="/">
-        <Image src={theplug} alt="logo" width={74} height={29} />
+       <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
